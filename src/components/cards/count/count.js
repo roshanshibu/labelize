b8cc5e516253321941ed890345e3fda209340951
Roshan Shibu <roshanshibu97@gmail.com>
2023-11-12 15:29:01 +0100

    dash + countCard

---
+++ src/components/cards/count/count.js
@@ -0,0 +1,22 @@
+import "../dashCard.css";
+import "./count.css";
+
+const CountCard = (props) => {
+  return (
+    <div className="dashCard">
+      <div className="cardContainer">
+        <div className="countText">
+          {props.count}
+          <span style={{ paddingBottom: "15px" }}>⭐</span>
+        </div>
+        <p className="subtitle1">images annotated</p>
+        <p className="subtitle2">
+          You are in the top <span>{props.percent} percentile</span> of
+          contributors
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CountCard;
--- src/pages/dash/dash.js
+++ src/pages/dash/dash.js
@@ -1,6 +1,16 @@
+import CountCard from "components/cards/count/count";
 import "./dash.css";
+import Branding from "assets/Branding.svg";
+
 const Dash = () => {
-  return <p>Dash</p>;
+  return (
+    <div className="dashContainer">
+      <img src={Branding} className="hide-desktop" />
+      <div className="cardsContainer">
+        <CountCard count={125} percent={12} />
+      </div>
+    </div>
+  );
 };
 
 export default Dash;
